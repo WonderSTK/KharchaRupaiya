d0d1538783b3822b4fb8dea1c98738526bc6d939
WonderSTK <mehulparmar9694@gmail.com>
2024-08-22 14:27:05 +0530

    modified

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -63,7 +63,7 @@ const ExpenseForm = (props) => {
           <input
             type='date'
             min='2019-01-01'
-            max='2023-12-31'
+            max='2025-12-31'
             value={enteredDate}
             onChange={dateChangeHandler}
           />
